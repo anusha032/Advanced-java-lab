3cbf72d3c046ee97480988597b938321f52c3ea2
anusha032 <naikanusha918@gmail.com>
2026-04-19 21:06:39 +0530

    Add files via upload

---
+++ p14.java
@@ -0,0 +1,22 @@
+// * Write a Java Program for Checking if a string reads the same backward as forward (ignoring case and punctuation) using user defined function isPalindrome():
+
+
+package lab3;
+
+import java.util.Scanner;
+
+public class p14 {
+
+	// 4. Function to check if a string is a palindrome
+	public static boolean isPalindrome(String str) {
+	if (str == null) {
+	return false;
+	}
+	String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+	return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
+	}
+
+	public static void main(String[] args) {
+	System.out.println("4. Palindrome: " + isPalindrome("A man, a plan, a canal: Panama"));
+	}
+}
