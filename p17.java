0ec8f28a5633c9acc3cab37179f67ad0e6de8d6f
anusha032 <naikanusha918@gmail.com>
2026-04-19 21:14:30 +0530

    Add files via upload

---
+++ p17.java
@@ -0,0 +1,20 @@
+//*  Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user defined function truncate()
+
+
+package lab3;
+import java.util.Scanner;
+
+public class p17 {
+
+	// 7. Function to truncate a string to a specified length
+	public static String truncate(String str, int maxLength) {
+	if (str == null) {
+	return null;
+	}
+	return str.length() > maxLength ? str.substring(0, maxLength) + "..." : str;
+	}
+
+	public static void main(String[] args) {
+	System.out.println("7. Truncate: " + truncate("Hello World", 5));
+	}
+}
