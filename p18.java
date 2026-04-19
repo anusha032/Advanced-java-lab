fb5c5a4e0369de47df65a535447ef546cf94d51e
anusha032 <naikanusha918@gmail.com>
2026-04-19 21:24:25 +0530

    Add files via upload

---
+++ p18.java
@@ -0,0 +1,19 @@
+//* Write a Java Program for Verifying if a string contains only numeric characters using user defined
+//* function isNumeric()
+
+package lab3;
+import java.util.Scanner;
+
+public class p18 {
+	// 8. Function to check if a string contains only digits
+	public static boolean isNumeric(String str) {
+	if (str == null || str.isEmpty()) {
+	return false;
+	}
+	return str.matches("\\d+");
+	}
+
+	public static void main(String[] args) {
+	System.out.println("8. Is Numeric: " + isNumeric("12345"));
+	}
+}
